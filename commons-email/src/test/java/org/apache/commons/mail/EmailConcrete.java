ce6a274597224205c243a7e3b857a3638910e96b
jannatul fardows <jfardows96@gmail.com>
2020-11-17 22:13:43 -0500

    i add two classes for test AddBcc().

--- commons-email/src/test/java/org/apache/commons/mail/EmailConcrete.java
+++ commons-email/src/test/java/org/apache/commons/mail/EmailConcrete.java
@@ -1,5 +1,9 @@
 package org.apache.commons.mail;
 
-public class EmailConcrete {
-
+public class EmailConcrete extends  Email{
+	@Override 
+	public Email setMsg(String msg) throws EmailException{
+		return null;
+		
+	}
 }
--- commons-email/src/test/java/org/apache/commons/mail/EmailTest.java
+++ commons-email/src/test/java/org/apache/commons/mail/EmailTest.java
@@ -1,5 +1,37 @@
 package org.apache.commons.mail;
 
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
 public class EmailTest {
+
+	
+	private static final String[] TEST_EMAILS = { "abbccc@.com", "a.b@c.org", "jjjjk@b.com"};
+	
+	private EmailConcrete email;
+	
+	@Before
+	public void setUpEmailTest() throws Exception{
+		
+		email = new EmailConcrete();
+	}
+	 @After
+	 public void tearDownEmailTest() throws Exception{
+		 
+	 }
+	 
+	 @Test
+	 public void testAddBcc() throws Exception{
+		 
+		 
+		 email.addBcc(TEST_EMAILS);
+		 assertEquals(3, email.getBccAddresses().size());
+	 }
+	 
+
 
 }
