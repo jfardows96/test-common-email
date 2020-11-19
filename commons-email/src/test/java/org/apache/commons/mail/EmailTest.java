679052063444e635c06ada12ae7eb46d60196f0b
jannatul fardows <jfardows96@gmail.com>
2020-11-18 21:17:22 -0600

    make changed method AddReplyTo() to test this method

--- commons-email/src/test/java/org/apache/commons/mail/EmailTest.java
+++ commons-email/src/test/java/org/apache/commons/mail/EmailTest.java
@@ -52,5 +52,11 @@ public class EmailTest {
 		
 		assertEquals(1, email.headers.size());
 	}
+	@Test
+	public void testAddReplyTo() throws Exception{
+		
+		email.addReplyTo(TEST_EMAILS2, "Jannatul");
+		assertEquals(1, email.getReplyToAddresses().size());
+	}
 	
 }
